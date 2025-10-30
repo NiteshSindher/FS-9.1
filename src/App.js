00d9cfb82b1f3d07f7b8233c55af36e9895049aa
NiteshSindher <sindhernitesh2005@gmail.com>
2025-10-30 15:09:49 +0530

    Add initial React app with Docker support

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This React app is served using Nginx inside a Docker container!</p>
+    </div>
+  );
+}
+
+export default App;
